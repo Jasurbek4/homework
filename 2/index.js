78b63cf8a56fe09968a6fe0a44eb757ffcab1daf
unknown <jasuranarbekov99@gmail.com>
2023-11-03 09:23:35 +0500

    2-uyga vazifa

---
+++ 2/index.js
@@ -0,0 +1,49 @@
+//----1-vazifa--------
+
+// console.log(5 > 4)
+// console.log("apple" > "pineapple")
+// console.log("2" > "12")
+// console.log(undefined == null)
+// console.log(undefined === null)
+// console.log(null == "\n0\n")
+// console.log(null === +"\n0\n")
+
+
+//----2-vazifa--------
+
+
+// let a = 1, b=1;
+
+// let c = ++a;
+// let d = b++;
+
+// console.log(c)
+// console.log(d)
+
+
+//----3-vazifa--------
+
+
+// let a = 2;
+
+// let x = 1 + (a *= 2);
+// console.log(x)
+
+
+//----4-vazifa--------
+
+
+// console.log("" + 1 + 0)
+// console.log("" - 1 + 0)
+// console.log(true + false)
+// console.log(6/"3")
+// console.log("2" * "3")
+// console.log(4 + 5 + "px")
+// console.log("$" + 4 + 5)
+// console.log("4" - 2)
+// console.log("4px" - 2)
+// console.log(" -9 " + 5)
+// console.log(" - 9 " - 5)
+// console.log(null + 1)
+// console.log(undefined + 1)
+console.log(" \t \n" - 2)
