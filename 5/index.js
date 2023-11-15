3cb11dc75509d082b2fdadccbbab13714d3fc3d2
Jasurbek4 <jasuranabekov99@gmail.com>
2023-11-15 10:03:32 +0500

    5 - uyga vazifa Object | Basic

---
+++ 5/index.js
@@ -0,0 +1,24 @@
+//-------1-masala---------
+
+// let user = {
+//     name: "John",
+//     surname: "Smith",
+// }
+// user.name = "Pate";
+// console.log(user);
+
+
+//-------2-masala---------
+
+// let salaries = {
+//     John: 100,
+//     Ann: 160,
+//     Pate: 130
+// }
+
+// let sum = 0;
+// for (key in salaries) {
+//   sum = sum + salaries[key];
+// }
+
+// console.log(sum);
