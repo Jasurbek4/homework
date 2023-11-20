6faa6fa5e24f4a2d94b15f1ae7c43cefca8e9ce9
Jasurbek4 <jasuranabekov99@gmail.com>
2023-11-20 09:43:17 +0500

    6 - uyga vazifa Number

---
+++ 6/index.js
@@ -0,0 +1,42 @@
+//------1-masala-------------
+
+// function rand (a , b){
+//     console.log(Math.trunc(Math.random() * (b - a)) + a);
+// }
+
+// rand(5, 12)
+
+
+//------2-masala----------
+
+// function sign (a) {
+//     if (a > 0) {
+//         return 1;
+//     }
+//     else if (a < 0) {
+//         return -1;
+//     }
+//     else return 0;
+// }
+
+// console.log(sign(0));
+
+
+//------3-masala----------
+
+
+// function sign (a) {
+//     if (a > 0) {
+//         return a;
+//     }
+//     else if (a < 0) {
+//         return a *= -1;
+//     }
+// }
+
+// console.log(sign(10));
+
+
+//------4-masala----------
+
+
